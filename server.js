8b2cee2f50747046589f5e093a365f2e03fc0d33
Sunil Baghel <sunilbaghel93100@gmail.com>
2025-06-20 11:11:06 +0530

    server.js updated, home page added, dependencies installed

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
